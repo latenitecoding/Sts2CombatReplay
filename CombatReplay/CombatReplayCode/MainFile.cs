905cc110b0e1e34277e0a9b1a4bcbf3bc5b896af
latenitecoding <latenitecode@gmail.com>
2026-05-27 13:49:58 -0500

    adds timestamp to mod version

--- CombatReplay/CombatReplayCode/MainFile.cs
+++ CombatReplay/CombatReplayCode/MainFile.cs
@@ -12,8 +12,8 @@ using Tracker;
 public partial class MainFile : Node
 {
     private const string ModId = "CombatReplay"; //At the moment, this is used only for the Logger and harmony names.
-    public const string GameVersion = "v0.106.1 (2026.05.23)";
-    public const string ModVersion = "v1.7.2";
+    public const string GameVersion = "[v0.106.1] (2026.05.23)";
+    public const string ModVersion = "[v1.7.2] (2026.05.27)";
 
     public static MegaCrit.Sts2.Core.Logging.Logger Logger { get; } =
         new(ModId, MegaCrit.Sts2.Core.Logging.LogType.Generic);
